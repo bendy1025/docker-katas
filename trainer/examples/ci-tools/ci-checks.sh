e37754ed65eb1b44bb8316d453bd5973bb602ca7
Marben Israel <109124673+bendy1025@users.noreply.github.com>
2025-03-09 13:57:14 -0600

    Update ci-checks.sh

--- trainer/examples/ci-tools/ci-checks.sh
+++ trainer/examples/ci-tools/ci-checks.sh
@@ -6,6 +6,9 @@ docker run --rm -i hadolint/hadolint < Dockerfile-python
 docker run --rm -i hadolint/hadolint < Dockerfile-python-alpine
 
 echo "Scanning images with Trivy..."
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v aquacache:/root/.cache aquasec/trivy image flaskapp:ubuntu
-docker run --rm -v aquacache:/root/.cache aquasec/trivy image flaskapp:python3
-docker run --rm -v aquacache:/root/.cache aquasec/trivy image flaskapp:python3-alpine
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v aquacache:/root/.cache aquasec/trivy image flask-app:ubuntu
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v aquacache:/root/.cache aquasec/trivy image flask-app:python3
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v aquacache:/root/.cache aquasec/trivy image flask-app:python3-alpine
+
+# docker run --rm -v aquacache:/root/.cache aquasec/trivy image flaskapp:python3
+# docker run --rm -v aquacache:/root/.cache aquasec/trivy image flaskapp:python3-alpine
