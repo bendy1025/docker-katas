f882773fc05f330df305625ccfb42d82de600726
Marben Israel <109124673+bendy1025@users.noreply.github.com>
2025-03-09 09:28:58 -0600

    Update build.sh

--- trainer/examples/building-flask-on-different-os/build.sh
+++ trainer/examples/building-flask-on-different-os/build.sh
@@ -1,4 +1,4 @@
 # /bin/bash
-docker build -t flask-app .
-docker build -f Dockerfile-python -t flask-app:python .
-docker build -f Dockerfile-python-alpine -t flask-app:alpine .
+docker build -t flask-app:ubuntu .
+docker build -f Dockerfile-python -t flask-app:python3 .
+docker build -f Dockerfile-python-alpine -t flask-app:python3-alpine .
