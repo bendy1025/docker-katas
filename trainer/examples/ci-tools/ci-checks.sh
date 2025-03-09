cf7244cc3d82c4b4d719a4ccc233e8ca5ea6339c
Marben Israel <109124673+bendy1025@users.noreply.github.com>
2025-03-09 11:25:54 -0600

    Create ci-checks.sh

---
+++ trainer/examples/ci-tools/ci-checks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Running Hadolint on all Dockerfiles..."
+docker run --rm -i hadolint/hadolint < Dockerfile
+docker run --rm -i hadolint/hadolint < Dockerfile-python
+docker run --rm -i hadolint/hadolint < Dockerfile-python-alpine
+
+echo "Scanning images with Trivy..."
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v aquacache:/root/.cache aquasec/trivy image flaskapp:ubuntu
+docker run --rm -v aquacache:/root/.cache aquasec/trivy image flaskapp:python3
+docker run --rm -v aquacache:/root/.cache aquasec/trivy image flaskapp:python3-alpine
